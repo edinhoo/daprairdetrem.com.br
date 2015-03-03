ec2ad786cb4d9924479fe65a376eee7e441068fb
Harrison Mendonça <h@harrison.com.br>
2015-03-03 12:28:21 -0300

    pagina de login

---
+++ templates/login.php
@@ -0,0 +1,15 @@
+<section>
+
+<h1>Login</h1>
+
+<form>
+	<label>
+		email: <input type="text" name="email">
+	</label>
+	<label>
+		senha: <input type="text" name="senha">
+	</label>
+	<input type="submit">
+</form>
+
+</section>
